f5dcd49b0cafd67b25052a75f6a8cd287756dc25
vighnesh <vighneshbuddhivant10@gmail.com>
2022-12-13 05:21:11 +0000

    solution

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,6 @@
+let myArray = ["camel", 265, true, "5.6"];
+let bulb = { watts: 10, type: "LED" };
+function multipleByFour(num) {
+  return num * 4;
+}
+export { myArray, bulb, multipleByFour };
---
+++ importMultipleValues.mjs
@@ -0,0 +1,4 @@
+import { myArray, bulb, multipleOfFour } from "./exportMultipleValues.mjs";
+console.log(myArray);
+console.log(bulb);
+console.log(multipleByFour(6));
